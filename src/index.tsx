4a48156f95bbadb60b63f8c5d028c85a2c162baa
Ruben <chickenzrage@gmail.com>
2021-06-16 20:48:05 +0100

    Try to improve scailing?

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom';
 import _App from './components/app';
 import "./index.scss";
 
+// https://css-tricks.com/the-trick-to-viewport-units-on-mobile/
+let vh = window.innerHeight * 0.01;
+document.documentElement.style.setProperty('--vh', `${vh}px`);
+
 class App extends React.Component {
     render() {
         return (
